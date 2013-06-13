a8b666bf9ade406d2488e7721533d14eb3ba9a2e
edom18 <edo.m18@gmail.com>
2013-06-13 11:27:56 +0900

    added test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
-var assert = require('assert');
+(function () {
 
-describe('mochaのテスト', function () {
-    it('1 + 1は2になること', function () {
-        assert.equal(1 + 1, 2);
+    var assert = require("assert");
+
+    describe('Model test', function() {
+        it('get("string")で値が取り出せる', function() {
+            var model = new Model();
+            model.set('hoge', 'fuga');
+            assert('fuga', model.get('hoge'));
+        });
     });
-});
+}());
