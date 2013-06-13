069a9120b6038283fa7da301aa0fbabe9f4fd375
edom18 <edo.m18@gmail.com>
2013-06-13 11:28:31 +0900

    bug fixed a few undefined.

--- Model.js
+++ Model.js
@@ -15,19 +15,103 @@
             this.id = (attribute.id) ? attribute.id : modelIdBase + (++modelIdIndex);
 
             if (this.defaults) {
-                copyClone(attribute, this.defaults);
+                util.copyClone(attribute, this.defaults);
             }
 
             //set data by defaults and argumetns.
             this.set(attribute, {silent: true});
 
             //copy to `_previousAttributes` current attributes.
-            this._previousAttributes = copyClone({}, this.attributes);
+            this._previousAttributes = util.copyClone({}, this.attributes);
 
             //called `initialize` function if that exist on attributes.
             if (util.isFunction(this.initialize)) {
                 this.initialize.apply(this, arguments);
             }
+        },
+
+        /**
+         * @returns {Boolean} return true if model has been changed.
+         */
+        hasChanged: function () {
+            return !util.isEmpty(this._changed);
+        },
+
+        /**
+         * change attributes
+         */
+        change: function (options) {
+
+            var key, ret;
+
+            options || (options = {});
+
+            if (this._changing || !this.hasChanged()) {
+                return false;
+            }
+
+            this._changing = true;
+
+            //fired `change` event that takes changed object.
+            if (!options.silent) {
+                this.trigger('change', this._changed);
+
+                //fired event of each paramaters.
+                for (key in this._changed) {
+                    ret = {};
+                    ret[key] = this._changed[key];
+                    this.trigger('change:' + key, ret);
+                }
+            }
+
+            //copy `this.attributes` to `this._previousAttributes`.
+            this._previousAttributes = util.copyClone({}, this.attributes);
+
+            //delete changed object.
+            this._changed = null;
+            delete this._changed;
+
+            this._changing = false;
+        },
+        set: function (name, val, options) {
+        
+            var attr, attrs,
+                attributes = (this.attributes || (this.attributes = {}));
+
+            this._changed || (this._changed = {});
+
+            if (util.isString(name)) {
+                attrs = {};
+                attrs[name] = val;
+            }
+            else {
+                attrs = name;
+                options = val;
+            }
+
+            for (attr in attrs) {
+                if (attributes[attr] === attrs[attr]) {
+                    continue;
+                }
+                attributes[attr] = attrs[attr];
+                this._changed[attr] = attrs[attr];
+            }
+
+            if (this.hasChanged()) {
+                this.change(options);
+            }
+        },
+        previous: function (attr) {
+            if (!arguments.length || !this._previousAttributes) {
+                return null;
+            }
+            return this._previousAttributes[attr];
+        },
+        previousAttributes: function () {
+            return util.copyClone({}, this._previousAttributes);
+        },
+        get: function (name) {
+            return this.attributes[name];
         }
     });
 
