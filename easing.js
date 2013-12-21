f4c3d0eb03c409446b29bf3fcd50a893e76a7df6
edom18 <edo.m18@gmail.com>
2013-12-22 00:35:11 +0900

    added new methods.

--- easing.js
+++ easing.js
@@ -16,7 +16,9 @@
     var sin = Math.sin,
         cos = Math.cos,
         pow = Math.pow,
-        sqrt = Math.sqrt;
+        sqrt = Math.sqrt,
+        PI  = Math.PI,
+        hPI = PI / 2;
 
     var easing = {
 
@@ -28,6 +30,39 @@
 
         linear: function (a, b, x) {
             return a * (1.0 - x) + b * x;
+        },
+
+        easeInCubic: function (a, b, x) {
+            var c = b - a;
+            return c * pow(x, 3) + a;
+        },
+        easeOutCubic: function (a, b, x) {
+            var c = b - a;
+            return c * (pow(x - 1, 3) + 1) + a;
+        },
+        easeInOutCubic: function (a, b, x) {
+            var c = b - a;
+            var t = x * 2;
+
+            if (t < 1) {
+                return c / 2 * pow(t, 3) + a;
+            }
+            return c / 2 * (pow(t - 2, 3) + 2) + a;
+        },
+
+        easeInSine: function (a, b, x) {
+            var c = b - a;
+            return c * (1 - cos(x * (hPI))) + a;
+        },
+
+        easeOutSine: function (a, b, x) {
+            var c = b - a;
+            return c * sin(x * (hPI)) + a;
+        },
+
+        easeInOutSine: function (a, b, x) {
+            var c = b - a;
+            return c / 2 * (1 - cos(PI * x)) + a;
         },
 
         easeInOutExpo: function (a, b, x) {
